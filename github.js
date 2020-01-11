291316371404caee53c9a09e19a4727dc80f29ea
Mike Rivera <michaelstephenrivera@gmail.com>
2020-01-10 17:10:53 -0800

    Created logic for fetching and displaying repos in UI

--- github.js
+++ github.js
@@ -2,15 +2,21 @@ class Github {
   constructor() {
     this.client_id = '93b57be0978ab02e53ab';
     this.client_secret = 'f51221d40da8463cb8aa103ad8b77502b2354632';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile
+      profile,
+      repos
     }
   }
 }
