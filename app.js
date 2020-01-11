3f2dc875eed96086a55802e4565bfdef269c7993
Mike Rivera <michaelstephenrivera@gmail.com>
2020-01-10 16:55:43 -0800

    Created logic for displaying alert msg when profile is not found

--- app.js
+++ app.js
@@ -16,14 +16,15 @@ searchUser.addEventListener('keyup', (e) => {
     github.getUser(userText)
       .then(data => {
         if (data.profile.message === 'Not Found') {
-          // TODO: Show alert
-
+          // Show alert
+          ui.showAlert('User not found', 'alert alert-danger');
         } else {
-          // TODO: Show profile
+          // Show profile
           ui.showProfile(data.profile);
         }
       })
   } else {
-    // TODO: Clear profile
+    // Clear profile
+    ui.clearProfile();
   }
 });
